7afe0b36894e206ea8beed725dbb33857683b54a
olgayanes <olgatyanesp@gmail.com>
2015-12-06 21:50:46 +0100

    Create GestorFigurasTest.java

---
+++ Figuras/tests/GestorFigurasTest.java
@@ -0,0 +1,57 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GestorFigurasTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGestorFiguras() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGuardar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecuperar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCambiar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExiste() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecuperarLista() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecuperarIguales() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecuperarSemejantes() {
+		fail("Not yet implemented");
+	}
+
+}
